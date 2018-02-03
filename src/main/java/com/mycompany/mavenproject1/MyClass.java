86ce957e47afb956a4c6cfd9e4a3bd5b26df64c2
olexande <olexande@users.noreply.github.com>
2018-02-03 19:16:06 +0200

    add request to java class from jsp

---
+++ src/main/java/com/mycompany/mavenproject1/MyClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author oleksandr.bilovol
+ */
+public class MyClass {
+    public String testMethod(){
+        //System.out.println("Hello9999999999");
+        return  ("Hello9999999999");
+    }
+}
